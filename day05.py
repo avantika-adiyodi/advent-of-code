8a8aa2d0b7619d638743013b96b5f22f2eb5e8e8
avantika-adiyodi <avantika.adiyodi@gmail.com>
2026-04-12 12:09:51 +0530

    solved part 2 of day 5

--- day05.py
+++ day05.py
@@ -20,26 +20,6 @@ def split_database_inputs(content: str) -> list[str]:
     ids = content[sep+2:].split("\n")
     return ranges, ids
 
-def find_fresh_ingredients (fresh_range_str: list[str], ingredient_ids: list[str]) -> int:
-
-    ranges_str = []
-    fresh_ingredients = 0
-
-    for range_str in fresh_range_str:
-        start, end = range_str.split("-")
-        tuple_ = (int(start), int(end))
-        ranges_str.append(tuple_)
-
-    fresh_range = merge_intervals(ranges_str)
-    # print(f"fresh range = {fresh_range}")
-
-    for ingredient in ingredient_ids:
-        if contains (fresh_range, int(ingredient)):
-            fresh_ingredients += 1
-
-    
-    
-    return fresh_ingredients
 
 def merge_intervals(ranges: list[tuple]):
     ranges.sort()
@@ -53,7 +33,6 @@ def merge_intervals(ranges: list[tuple]):
     return merged
 
 
-
 def contains(merged: list, x: int) -> bool:
     # find position where x would go
     i = bisect.bisect_right(merged, [x, float('inf')]) - 1
@@ -64,7 +43,44 @@ def contains(merged: list, x: int) -> bool:
     
     return False
 
+
+def find_fresh_ingredients (fresh_range_str: list[str], ingredient_ids: list[str]) -> int:
+    ranges_str = []
+    available_fresh_ingredients = 0
+    total_fresh_ingredients = 0
+
+    # get the range of fresh ingredients
+    for range_str in fresh_range_str:
+        start, end = range_str.split("-")
+        tuple_ = (int(start), int(end))
+        ranges_str.append(tuple_)
+
+    fresh_range = merge_intervals(ranges_str)
+    # print(f"fresh_range[0] = {fresh_range[0]}")
+    # print(f"fresh range = {fresh_range}")
+    # print(f"{(range_[0], range_[1]) for range_ in fresh_range}")
+
+    # get count of total fresh ingredients
+    for range_ in fresh_range:
+        start = range_[0]
+        end = range_[1]
+        length = len(range(start, end+1))
+        total_fresh_ingredients += length
+        # total_fresh_ingredients += len(range(range_[0], range_[1]))
+        pass
+
+    # get count of available fresh ingredients
+    for ingredient in ingredient_ids:
+        if contains (fresh_range, int(ingredient)):
+            available_fresh_ingredients += 1
+
+    
+    
+    return available_fresh_ingredients, total_fresh_ingredients
+
+
 if __name__ == "__main__":
+    print("--- Day 5: Cafeteria ---")
     if example:
         input_file = input_file_ex
         print("\nMain: Running with example input.")
@@ -82,7 +98,7 @@ if __name__ == "__main__":
 
     print("Main: got the ingredients database.\n")
 
-    fresh_ingredients = find_fresh_ingredients(ranges_str, ingredientIDs_str)
+    fresh_ingredients, total_ingredients = find_fresh_ingredients(ranges_str, ingredientIDs_str)
 
-    print(f"Main: (part 1) total number of fresh ingredients = {fresh_ingredients}")
-    # print(f"Main: (part 2)  = {}")
+    print(f"Main: (part 1) total number of available fresh ingredients = {fresh_ingredients}")
+    print(f"Main: (part 2) total number of fresh ingredients = {total_ingredients}")
